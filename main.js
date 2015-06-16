faace18b5e603690114f63b081f696f4874c14d2
Wasiak <qbawasiak@wp.pl>
2015-06-16 18:51:26 +0200

    new post fixed

--- main.js
+++ main.js
@@ -26,7 +26,8 @@ var Post = function (title, text) {
 var postTitle = document.getElementById('post-title');
 var postContent = document.getElementById('textarea');
 
-var addNewPost = function() {
+var addNewPost = function(event) {
+  event.preventDefault();
   if (postTitle.value && postContent.value) {
     new Post(postTitle.value, postContent.value);
   }
@@ -36,3 +37,5 @@ var button = document.getElementById('button');
 button.addEventListener('click', addNewPost);
 new Post('tytuł probny', 'text powitalny na probe');
 new Post('tytul 2', 'tekst posta 22222222222');
+
+// nie z forma tylko z post.js jako obiekty tak jak w king books

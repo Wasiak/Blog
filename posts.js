8ee436a686ac4cd255423d6816c29cde5c1b8202
Wasiak <qbawasiak@wp.pl>
2015-06-21 23:36:43 +0200

    post 3

--- posts.js
+++ posts.js
@@ -12,5 +12,16 @@ var posts = [
               I\'m going to update blog regular and I hope I will handle it. During work on this blog I learned \
               new thing for me: appending element to the DOM after another chosen element with "insertBefore" method. \
               I\'m curious how many people will read my reflections.'
+  },
+  {
+    'date' : '21/06/2015',
+    'title' : 'First comercial project',
+    'text' : 'This week I finished my first comercial project. Thanks to my friend Michał, who is my mentor and for now my boss ;) \
+              He helped me in coding harder parts of code and again gave my countless advices and tips. During this project I\'ve been using \
+              Fabric.js framework to draw elements on canvas and make them movable, rotatalbe and scalable. I learned many new technics of coding \
+              and knew many new options available in JavaScript. One of completely new thing for me was writing a requests to comunicate with server. \
+              Till now I have no idea how it works but now I understand it, of course I know only basic way of using it but next time I will \
+              use requests it will take me much less time to code it correctly. In some next post I will write about some problems which I had \
+              during this project and ways of solving it with specific example.'
   }
 ];

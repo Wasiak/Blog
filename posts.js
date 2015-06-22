c25e93bf60645d0d1f56ce211af773a689ba1a3c
Wasiak <qbawasiak@wp.pl>
2015-06-22 20:23:53 +0200

    post 4

--- posts.js
+++ posts.js
@@ -23,5 +23,18 @@ var posts = [
               Till now I have no idea how it works but now I understand it, of course I know only basic way of using it but next time I will \
               use requests it will take me much less time to code it correctly. In some next post I will write about some problems which I had \
               during this project and ways of solving it with specific example.'
+  },
+  {
+    'date' : '22/06/2015',
+    'title' : 'Border collision in fabric.js',
+    'text' : 'I just wanna say few words about problem I had in last project. I have few objects on canvas (fabric canvas).\
+              Framework knows them as fabric.Objects. Problem was about colliding objects with borders of canvas. I couldn\'t \
+              find in any fabric tutorial or documents function detecting this collision. But I found function detecting collision \
+              between objects on canvas. So after severals trials I thought about using border as a object. Unfortunately it didn\'t \
+              works. When I was sitting and thinking about it I saw a fly which want to escape from room through the window \
+              but curtain stoped it. I thought  that this is "invisible" border of room. Then I got an answer. I draw on canvas \
+              4 lines along the borders of canvas with transparent color and now I could use fabric objects collision detection \
+              function to check if any of my objects is collidin with any of my 4 lines/borders which are fabric objects to.\
+              It works perfect.  I\'m proud of myself because of this idea ;)'
   }
 ];
